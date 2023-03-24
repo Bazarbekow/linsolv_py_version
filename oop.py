7ec8692332de69f9637ab2efe980b578c6c68ea5
Akha <bazar.akhmet@gmail.com>
2023-03-24 17:43:31 +0100

    Big Update

--- oop.py
+++ oop.py
@@ -27,7 +27,7 @@ class LinearEquationSolver(QMainWindow):
         self.grid_layout.addWidget(self.n_size_spinbox, 1, self.n_size + 5)
 
         result_label = QLabel("")
-        self.grid_layout.addWidget(result_label,self.m_size + 3,self.n_size)
+        self.grid_layout.addWidget(result_label,self.m_size + 2,self.n_size + 4)
 
         self.m_size_label = QLabel("Rows of A matrix:")
         self.grid_layout.addWidget(self.m_size_label, 0, self.n_size + 4)
@@ -36,20 +36,21 @@ class LinearEquationSolver(QMainWindow):
         self.grid_layout.addWidget(self.n_size_label, 0, self.n_size + 5)
 
         self.create_entries(self.m_size, self.n_size)
-
+        
         solve_button = QPushButton("Solve")
         solve_button.clicked.connect(self.on_solve)
         self.grid_layout.addWidget(solve_button, self.m_size, self.n_size + 4)
 
         create_entries_button = QPushButton("Create Entries")
         create_entries_button.clicked.connect(lambda: self.create_entries(self.m_size_spinbox.value(), self.n_size_spinbox.value()))
-        self.grid_layout.addWidget(create_entries_button, self.m_size + 2, self.n_size + 4)
+        self.grid_layout.addWidget(create_entries_button, self.m_size + 1, self.n_size + 4)
 
         label1 = QLabel("Matrix A")
         self.grid_layout.addWidget(label1, 0, 0, 1, self.n_size)
 
         label2 = QLabel("Vector b")
         self.grid_layout.addWidget(label2, 0, self.n_size)
+
 
     def create_entries(self, m_size, n_size):
         self.entries.clear()
@@ -64,12 +65,16 @@ class LinearEquationSolver(QMainWindow):
             for j in range(n_size):
                 entry = QLineEdit("0")
                 entry.textChanged.connect(self.clear_zero)
-                self.grid_layout.addWidget(entry, i + 1, j)
+                self.grid_layout.addWidget(entry, i+1, j)
                 self.entries[i].append(entry)
+
             entry = QLineEdit("0")
             entry.textChanged.connect(self.clear_zero)
-            self.grid_layout.addWidget(entry, i + 1, n_size)
+            self.grid_layout.addWidget(entry, i+1, n_size)
             self.b_entries.append(entry)
+
+        self.m_size = m_size
+        self.n_size = n_size
             
     def clear_zero(text):
         if text == "0":
@@ -86,13 +91,13 @@ class LinearEquationSolver(QMainWindow):
 
     def on_solve(self):
         try:
-            m_size = self.m_size_spinbox.value()
-            n_size = self.n_size_spinbox.value()
+            m_size = self.m_size
+            n_size = self.n_size
             A = [[float(self.entries[i][j].text()) for j in range(n_size)] for i in range(m_size)]
             b = [float(self.b_entries[i].text()) for i in range(m_size)]
             x = self.solve_linsys(A, b)
             result_label = self.central_widget.findChild(QLabel, "")
-            result_label.setText(str(x))
+            result_label.setText("Solution set: " + str(x))
         except ValueError:
             QMessageBox.critical(self, "Error", "Input is invalid. Please enter the correct matrix and vector.")
 
